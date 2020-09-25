e85430d372e2886c96de2381f07d172017c02178
tbwang99 <49505794+tbwang99@users.noreply.github.com>
2020-09-25 16:46:23 -0400

    even uglier

--- feedback/models.py
+++ feedback/models.py
@@ -28,7 +28,7 @@ class Feedback(models.Model):
     field1 = models.CharField(max_length = 200, validators =[validate_first] )
     field2 = models.PositiveSmallIntegerField(validators=[MinValueValidator(501), MaxValueValidator(1340)])
     field3 = models.CharField(max_length = 200, validators =[validate_third] )
-    field4 = models.CharField(max_length = 200, validators =[validate_fourth] )
+    field4 = models.CharField(max_length = 200, validators =[validate_fourth]  )
     name = models.CharField(max_length = 200)
     def __str__(self):
         return self.name
